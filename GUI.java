2a51a6fc7bbcaf93f61a5bc0b1ee367faa79c802
User <user@Users-MBP.hsd1.co.comcast.net>
2023-12-22 13:46:22 -0700

    Java GUI

--- GUI.java
+++ GUI.java
@@ -1,10 +1,13 @@
-import java
+import javax.swing.JOptionPane;
 
 class GUI {
-    public static void(String[] args){
+    public static void main(String[] args){
 
+        String name = JOptionPane.showInputDialog("Enter Your Name");
+        JOptionPane.showMessageDialog(null, "Hello "+name);
 
-
+        int age = Integer.parseInt(JOptionPane.showInputDialog("Enter Your age"));
+        JOptionPane.showMessageDialog(null, "you are "+age+" years old");
 
     }
 }
