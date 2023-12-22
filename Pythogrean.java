d1ebe899b9f724bc1b22bdeed02e3efd6b54669c
User <user@Users-MBP.hsd1.co.comcast.net>
2023-12-22 14:14:56 -0700

    New Code

---
+++ Pythogrean.java
@@ -0,0 +1,20 @@
+import javax.swing.JOptionPane;
+
+
+public class Pythogrean {
+    public static void main(String[] args){
+
+        double z;
+    
+
+        int x = Integer.parseInt(JOptionPane.showInputDialog("Enter Side One"));
+
+        int y = Integer.parseInt(JOptionPane.showInputDialog("Enter Side two"));
+
+
+        z = Math.sqrt((x*x)*(y*y));
+
+        JOptionPane.showMessageDialog(null, "Side three is equal to "+z);
+
+    }
+}
