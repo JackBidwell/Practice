6d87e4ac2ff5c5d8f6705b3db5507ceb00b5ea2a
User <user@Users-MBP.hsd1.co.comcast.net>
2023-12-29 08:20:08 -0700

    Logical operators and looping

---
+++ LogicalOperators.java
@@ -0,0 +1,37 @@
+//*Assesing an intbetween statement using operating like && and || Or ! Not */
+import java.util.Scanner;
+
+public class LogicalOperators {
+
+    public static void main(String[] args){
+
+     /* an and statement 
+        int temp = 25;
+
+        if(temp>30){
+            System.out.println("It is hot outside");
+        }
+        if(temp<30 && temp>20){
+            System.out.println("It is warm outsidse");
+        }
+        if(temp<20){
+            System.out.println("It is cold outside");
+        }
+    */
+/* Or statement
+
+ Scanner scanner = new Scanner(System.in);
+
+        System.out.println("You are playing a game press q or Q to quit");
+        String response = scanner.next();
+
+        if(response.equals("q") || response.equals("Q")){
+            System.out.println("You quit the Game");
+        }
+        else{
+            System.out.println("You are still playing the game");
+        }
+*/
+    }
+    
+}
---
+++ Looping.java
@@ -0,0 +1,19 @@
+/*Make a loop by comparing equal repetetive values or opposing differnt values */
+
+import java.util.Scanner;
+
+
+public class Looping {
+
+    public static void main(String[] args){
+
+        while(1==1){
+            System.out.println("Help, Im stuck in a while loop");
+        }
+
+
+    }
+
+}
+    
+
