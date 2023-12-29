e9171a180c9a2adae089126466a630371eeb206e
User <user@Users-MBP.hsd1.co.comcast.net>
2023-12-29 08:47:37 -0700

    For loop

---
+++ For Loop.java
@@ -0,0 +1,13 @@
+/*A loop that will only repeat a set amount of times that you are aware of */
+
+class ForLoop {
+    
+    public static void main(String[] args){
+
+        for(int i = 0; i<=10; i++){
+            System.out.println("HI");
+        }
+
+    }
+
+}
